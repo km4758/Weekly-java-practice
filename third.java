dcf5a70a5c6d534948d29a7882f7d4cf1ddae4be
km4758 <km4758@srmist.edu.in>
2026-03-03 15:44:10 +0530

    level1 problems

---
+++ third.java
@@ -0,0 +1,12 @@
+public class third {
+      public static void main(String[] args) {
+        
+        double kilometers = 10.8;
+        double conversionFactor = 1.6;  // 1 km = 1.6 miles
+        
+        double miles = kilometers * conversionFactor;
+        
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+
+}
